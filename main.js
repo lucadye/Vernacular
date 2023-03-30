ccedbf1937ad05beecfbc64e7fe0b9a200ff1386
lucadye <121118370+lucadye@users.noreply.github.com>
2023-03-30 17:09:01 -0400

    Start support for Bezier curves

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ function vector2(x, y) {
 	};
 }
 
+function curve(start, end, cp1, cp2) {
+	return {
+		start,
+		end,
+		cp1,
+		cp2,
+		drawSelf(ctx) {
+			ctx.moveTo(
+				this.start.x * 10 + 10,
+				this.start.y * 10 + 10
+			);
+			ctx.bezierCurveTo(
+				this.cp1.x * 10 + 10,
+				this.cp1.y * 10 + 10,
+				this.cp2.x * 10 + 10,
+				this.cp2.y * 10 + 10,
+				this.end.x * 10 + 10,
+				this.end.y * 10 + 10
+			);
+			ctx.stroke();
+		}
+	};
+}
+
 function newLetterCanvas() {
 	let canvas = document.createElement('canvas');
 	canvas.class = 'letter';
@@ -37,6 +61,12 @@ function drawLetters(letters) {
 				});
 		}
 
+		if (letter.curves) {
+			letter.curves.forEach((curve) => {
+				curve.drawSelf(ctx);
+			});
+		}
+
 		section.appendChild(canvas);
 	});
 }
@@ -58,6 +88,7 @@ let letters = [
 				vector2(0, 0)
 			]
 		],
+		curves: []
 	},
 	{
 		lines: [
@@ -78,6 +109,23 @@ let letters = [
 				vector2(8, 4)
 			]
 		],
+		curves: []
+	},
+	{
+		lines: [
+			[
+				vector2(8, 0),
+				vector2(8, 5)
+			]
+		],
+		curves: [
+			curve(
+				vector2(8, 5),
+				vector2(4, 5),
+				vector2(8, 8),
+				vector2(4, 8)
+			)
+		]
 	},
 	{
 		lines: [
@@ -97,6 +145,20 @@ let letters = [
 				vector2(rand(), rand()),
 				vector2(rand(), rand())
 			]
+		],
+		curves: [
+			curve(
+				vector2(rand(), rand()),
+				vector2(rand(), rand()),
+				vector2(rand(), rand()),
+				vector2(rand(), rand())
+			),
+			curve(
+				vector2(rand(), rand()),
+				vector2(rand(), rand()),
+				vector2(rand(), rand()),
+				vector2(rand(), rand())
+			)
 		]
 	}
 ];
